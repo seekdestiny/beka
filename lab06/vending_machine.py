58aff85fb5ed6c2cec05ee43fa053f3a2a715386
seekdestiny <jifeiqian@gmail.com>
2017-06-03 12:24:22 -0700

    complete lab06

---
+++ lab06/vending_machine.py
@@ -0,0 +1,27 @@
+def vending_machine(snacks):
+    """Cycles through list of snacks.
+
+    >>> vender = vending_machine(('chips', 'chocolate', 'popcorn'))
+    >>> vender()
+    'chips'
+    >>> vender()
+    'chocolate'
+    >>> vender()
+    'popcorn'
+    >>> vender()
+    'chips'
+    >>> other = vending_machine(('brownie',))
+    >>> other()
+    'brownie'
+    >>> vender()
+    'chocolate'
+    """
+    "*** YOUR CODE HERE ***"
+    count = 0
+    def counter():
+        nonlocal count
+        snack = snacks[count]
+        count = (count + 1) % len(snacks)
+        return snack
+    return counter
+
