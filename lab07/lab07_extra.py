c31c4d5eb49cf956538d169b73bdc1feee2a3b04
seekdestiny <jifeiqian@gmail.com>
2017-06-03 12:24:41 -0700

    complete lab07

---
+++ lab07/lab07_extra.py
@@ -0,0 +1,105 @@
+from lab07 import *
+
+# Q6
+def reverse_other(t):
+    """Reverse the roots of every other level of the tree using mutation.
+
+    >>> t = Tree(1, [Tree(2), Tree(3), Tree(4)])
+    >>> reverse_other(t)
+    >>> t
+    Tree(1, [Tree(4), Tree(3), Tree(2)])
+    >>> t = Tree(1, [Tree(2, [Tree(5, [Tree(7), Tree(8)]), Tree(6)]), Tree(3)])
+    >>> reverse_other(t)
+    >>> t
+    Tree(1, [Tree(3, [Tree(5, [Tree(8), Tree(7)]), Tree(6)]), Tree(2)])
+    """
+    "*** YOUR CODE HERE ***"
+    def help(s, need_reverse):
+        if s.is_leaf():
+            return
+        new_labs = [child.root for child in s.branches][::-1]
+        for i in range(len(s.branches)):
+            b = s.branches[i]
+            help(b, not need_reverse)
+            if need_reverse:
+                b.root = new_labs[i]
+    help(t, True)
+
+# Q7
+def cumulative_sum(t):
+    """Mutates t where each node's root becomes the sum of all entries in the
+    corresponding subtree rooted at t.
+
+    >>> t = Tree(1, [Tree(3, [Tree(5)]), Tree(7)])
+    >>> cumulative_sum(t)
+    >>> t
+    Tree(16, [Tree(8, [Tree(5)]), Tree(7)])
+    """
+    "*** YOUR CODE HERE ***"
+    for st in t.branches:
+        cumulative_sum(st)
+    t.root = sum([st. root for st in t.branches]) + t.root
+
+# Q8
+def deep_map_mut(fn, link):
+    """Mutates a deep link by replacing each item found with the
+    result of calling fn on the item.  Does NOT create new Links (so
+    no use of Link's constructor)
+
+    Does not return the modified Link object.
+
+    >>> link1 = Link(3, Link(Link(4), Link(5, Link(6))))
+    >>> deep_map_mut(lambda x: x * x, link1)
+    >>> print_link(link1)
+    <9 <16> 25 36>
+    """
+    "*** YOUR CODE HERE ***"
+    if link is Link.empty:
+        return
+    if isinstance(link.first, Link):
+        deep_map_mut(fn, link.first)
+    else:
+        link.first = fn(link.first)
+    deep_map_mut(fn, link.rest)
+
+# Q9
+def has_cycle(link):
+    """Return whether link contains a cycle.
+
+    >>> s = Link(1, Link(2, Link(3)))
+    >>> s.rest.rest.rest = s
+    >>> has_cycle(s)
+    True
+    >>> t = Link(1, Link(2, Link(3)))
+    >>> has_cycle(t)
+    False
+    >>> u = Link(2, Link(2, Link(2)))
+    >>> has_cycle(u)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    fast, slow = link, link
+    while fast is not Link.empty and fast.rest is not Link.empty:
+        fast, slow = fast.rest.rest, slow.rest
+        if fast == slow:
+            return True
+    return False
+
+def has_cycle_constant(link):
+    """Return whether link contains a cycle.
+
+    >>> s = Link(1, Link(2, Link(3)))
+    >>> s.rest.rest.rest = s
+    >>> has_cycle_constant(s)
+    True
+    >>> t = Link(1, Link(2, Link(3)))
+    >>> has_cycle_constant(t)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    fast, slow = link, link
+    while fast is not Link.empty and fast.rest is not Link.empty:
+        fast, slow = fast.rest.rest, slow.rest
+        if fast == slow:
+            return True
+    return False
