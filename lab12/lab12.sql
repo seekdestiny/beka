90db3fe3ba1d59efbc22963e5404f1c96d730afc
seekdestiny <jifeiqian@gmail.com>
2017-06-15 22:23:13 -0700

    complete lab12

---
+++ lab12/lab12.sql
@@ -0,0 +1,20 @@
+.read sp16data.sql
+.read fa16data.sql
+
+CREATE TABLE obedience AS
+  select seven, denero from students;
+
+CREATE TABLE smallest_int AS
+  select time, smallest from students where smallest > 8 order by smallest limit 20;
+
+CREATE TABLE greatstudents AS
+  select a.date, a.number, a.pet, a.color, b.color from students as a, sp16students as b where 
+          a.date = b.date and a.number = b.number and a.pet = b.pet;
+
+CREATE TABLE sevens AS
+  select s.seven from students as s, checkboxes as c where s.number = 7 and 
+  c.'7' = 'True' and s.time = c.time;
+
+CREATE TABLE matchmaker AS
+  select a.pet, a.song, a.color, b.color from students as a, students as b where a.time < b.time and
+  a.pet = b.pet and a.song = b.song;
